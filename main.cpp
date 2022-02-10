4574b448ed93bf9d99f88c3aee629782f413067e
PCristian00 <cristianp9300@gmail.com>
2022-02-10 14:46:22 +0100

    Esempio puntatori void (incrementa)

--- main.cpp
+++ main.cpp
@@ -1,45 +1,72 @@
 #include <iostream>
+
 using namespace std;
 
 //Restituisce il maggiore tra i due
-int maxn(int a, int b){
-    if(a>=b) return a;
+int maxn(int a, int b) {
+    if (a >= b) return a;
     return b;
 }
 
 //Restituisce un puntatore al maggiore tra i due
-int* max(int* pa, int* pb)
-{
+int *max(int *pa, int *pb) {
     if (*pa >= *pb) return pa;
     return pb;
 }
 
 //Restituisce l'indirizzo al maggiore tra i due
-int& max(int& ra, int& rb)
-{
+int &max(int &ra, int &rb) {
     if (ra >= rb) return ra;
     return rb;
 }
 
+//Incrementa ciò che riceve (char o int) di 1.
+void incrementa(void *data, int pdim) {
+    if (pdim == sizeof(char)) {
+        char *pchar;
+        pchar = (char *) data;
+        ++(*pchar);
+    }
+    else if (pdim == sizeof(int)) {
+        int *pint;
+        pint = (int *) data;
+        ++(*pint);
+    }
+}
 
-int main()
-{ int x, y;
-    cin>> x >> y;
+
+int main() {
+    int x = 5, y = 10;
+    //cin>> x >> y;
     // maxn(x,y)=0 non può essere compilato
     // La funzione non cambia i valori di x e y, è inutile per il nostro caso
-    int m=maxn(x,y);
-    m=0;
+    int m = maxn(x, y);
+    m = 0;
     cout << "x = " << x << " y = " << y << '\n';
 
-    cin >> x >> y;
+    cout << endl;
+
+    //cin >> x >> y;
     //Il puntatore al maggiore diventa zero -> il maggiore diventa zero
     *max(&x, &y) = 0;
     cout << "x = " << x << " y = " << y << '\n';
 
-    cin >> x >> y;
+    cout << endl;
+
+    //cin >> x >> y;
     //Il maggiore diventa zero
     max(x, y) = 0;
     cout << "x = " << x << " y = " << y << '\n';
+
+    cout << endl;
+
+    char a = 'x';
+    int b = 1602;
+    cout << a << ", " << b << '\n';
+    incrementa(&a, sizeof(a));
+    incrementa(&b, sizeof(b));
+    cout << a << ", " << b << '\n';
+
 
     return 0;
 }
